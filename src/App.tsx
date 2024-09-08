6b30adcacaec8f5ad1fa31f653386a4cfaf82701
developer <RahmanMoradiDev@gmail.com>
2024-09-08 16:02:57 +0330

    :tada: Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen bg-amber-300 flex items-center justify-center">
+        <p className='text-center items-center'>
+          Hello, Welcome To Rahman Moradi Personal Website!
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
