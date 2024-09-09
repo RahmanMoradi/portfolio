a59933b72a3893fd886d7639796ef35cc443b10e
developer <RahmanMoradiDev@gmail.com>
2024-09-09 16:06:55 +0330

    adding related icon and some fancy stuff

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,20 @@ function App() {
           <h1 className='p-1 text-center text-3xl bg-white font-mono'>
             Rahman Moradi
           </h1>
-          <p className='text-lg'>Based in Iran / Mashhad</p>
-          <p><span className='text-green-600 animate-ping'>●</span> Available For a Full-Time Position</p>
+          <div className='flex'>
+            <img src='images/location.png' className='w-6 h-6'></img>
+            <p className='text-lg'>Based in Iran / Mashhad</p>
+          </div>
+          <div className='flex'>
+            <span className="relative flex h-3 w-3">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+            </span>
+            <p> Available For a <b>Full-Time</b> Position</p>
+          </div>
         </div>
         <div className='my-8'>
-          <img src='src/assets/rahman.jpg' className='w-62 h-72 rounded-full'></img>
+          <img src='images/rahman.jpg' className='w-62 h-72 rounded-full'></img>
         </div>
         <div className='w-3/4 flex item-center justify-center text-center text-xl font-mono'>
           <Typewriter
